15d0d4e25bbcb5df3c25d194869404ef45b574f4
rupeshtomar <rupstomar@gmail.com>
2025-12-31 14:08:15 +0530

    Refactor service worker code for clarity

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,20 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2639
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+const CACHE_NAME = 'osint-workbench-v1';
 
-\f0\fs24 \cf0 const CACHE_NAME = 'osint-workbench-v1';\
-\
-const ASSETS = [\
-  './',\
-  './index.html',\
-  './manifest.json',\
-  './icon.svg'\
-];\
-\
-self.addEventListener('install', event => \{\
-  event.waitUntil(\
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))\
-  );\
-\});\
-\
-self.addEventListener('fetch', event => \{\
-  event.respondWith(\
-    caches.match(event.request).then(resp => resp || fetch(event.request))\
-  );\
-\});\
-}
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
